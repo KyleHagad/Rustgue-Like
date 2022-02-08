442615e1ff4f3e12487e9a93a7ac72de229d8c79
khagadorn <xekyle@gmail.com>
2022-02-08 16:19:35 -0700

    Build Floors and walls, add wall based movement obstruction

--- hellorust/src/main.rs
+++ hellorust/src/main.rs
@@ -22,6 +22,11 @@ struct LeftMover {}
 #[derive(Component)]
 struct Player {}
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+  Wall, Floor
+}
+
 struct State {
   ecs: World
 }
@@ -29,10 +34,14 @@ struct State {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
   let mut positions = ecs.write_storage::<Position>();
   let mut players = ecs.write_storage::<Player>();
+  let map = ecs.fetch::<Vec<TileType>>();
 
   for (_player, pos) in (&mut players, &mut positions).join() {
-    pos.x = min(79 , max(0, pos.x + delta_x));
-    pos.y = min(49, max(0, pos.y + delta_y));
+    let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+    if map[destination_idx] != TileType::Wall {
+      pos.x = min(79 , max(0, pos.x + delta_x));
+      pos.y = min(49, max(0, pos.y + delta_y));
+    }
   }
 }
 
@@ -49,6 +58,54 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
   }
 }
 
+pub fn xy_idx(x: i32, y: i32) -> usize {
+  (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+  let mut map = vec![TileType::Floor; 80*50];
+
+  for x in 0..80 {
+    map[xy_idx(x, 0)] = TileType::Wall;
+    map[xy_idx(x, 49)] = TileType::Wall;
+  }
+  for y in 0..50 {
+    map[xy_idx(0, y)] = TileType::Wall;
+    map[xy_idx(79, y)] = TileType::Wall;
+  }
+
+  let mut rng = rltk::RandomNumberGenerator::new();
+
+  for _i in 0..400 {
+    let x = rng.roll_dice(1, 79);
+    let y = rng.roll_dice(1, 49);
+    let idx = xy_idx(x, y);
+    if idx != xy_idx(40, 25) {
+        map[idx] = TileType::Wall;
+    }
+  }
+
+  map
+}
+
+fn draw_map(map: &[TileType], ctx : &mut Rltk) {
+  let mut x = 0;
+  let mut y = 0;
+  for tile in map.iter() {
+    match tile {
+      TileType::Floor => {
+        ctx.set(x,y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('·'))
+      }
+      TileType::Wall => {
+        ctx.set(x,y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('■'))
+      }
+    }
+
+    x += 1;
+    if x > 79 { x = 0; y += 1; }
+  }
+}
+
  //  GameState is a trait implemented on State ⚵
 impl GameState for State {
   //  Tick is a method on the trait GameState
@@ -58,6 +115,9 @@ impl GameState for State {
     self.run_systems();
 
     player_input(self, ctx);
+
+    let map = self.ecs.fetch::<Vec<TileType>>();
+    draw_map(&map, ctx);
 
     let positions = self.ecs.read_storage::<Position>();
     let renderables = self.ecs.read_storage::<Renderable>();
@@ -103,6 +163,8 @@ fn main() -> rltk::BError {
   gs.ecs.register::<Renderable>();
   gs.ecs.register::<LeftMover>();
   gs.ecs.register::<Player>();
+
+  gs.ecs.insert(new_map());
 
   gs.ecs.create_entity()
     .with(Position { x: 40, y: 25 })
