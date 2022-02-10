338a0cf9cb9f96ee6063c02449aa8b694e67834c
Kyle H <kyle.hagad@gmail.com>
2022-02-10 10:46:23 -0700

    Monster chase behavior

--- hellorust/src/monster_ai_system.rs
+++ hellorust/src/monster_ai_system.rs
@@ -1,20 +1,34 @@
 use specs::prelude::*;
-use super::{ Viewshed, Monster, Name };
+use super::{ Viewshed, Monster, Name, Map, Position };
 use rltk::{ Point, console };
 
 pub struct MonsterAI {}
 impl<'a> System<'a> for MonsterAI {
-    type SystemData = ( ReadExpect<'a, Point>,
-                        ReadStorage<'a, Viewshed>,
+    #[allow(clippy::type_complexity)]
+    type SystemData = ( WriteExpect<'a, Map>,
+                        ReadExpect<'a, Point>,
+                        WriteStorage<'a, Viewshed>,
                         ReadStorage<'a, Monster>,
-                        ReadStorage<'a, Name>  );
+                        ReadStorage<'a, Name>,
+                        WriteStorage<'a, Position>  );
 
     fn run(&mut self, data : Self::SystemData) {
-        let (player_pos, viewshed, monster, name) = data;
+        let (mut map, player_pos, mut viewshed, monster, name, mut position) = data;
 
-        for (viewshed, _monster, name) in (&viewshed, &monster, &name).join() {
+        for (mut viewshed, _monster, name, mut pos) in (&mut viewshed, &monster, &name, &mut position).join() {
             if viewshed.visible_tiles.contains(&*player_pos) {
                 console::log(&format!("{} sees you!", name.name));
+
+                let path = rltk::a_star_search(
+                    map.xy_idx(pos.x, pos.y) as i32,
+                    map.xy_idx(player_pos.x, player_pos.y) as i32,
+                    &mut *map
+                );
+                if path.success && path.steps.len() > 1{
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    viewshed.dirty = true;
+                }
             }
         }
     }
