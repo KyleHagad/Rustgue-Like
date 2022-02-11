7ac996fab6959dd4893c20d4dc7ec7c6231095d1
Kyle H <kyle.hagad@gmail.com>
2022-02-11 11:30:02 -0700

    Entity spawner

--- hellorust/src/map.rs
+++ hellorust/src/map.rs
@@ -3,9 +3,9 @@ use rltk::{ RandomNumberGenerator, Rltk, RGB, BaseMap, Algorithm2D, Point };
 use std::cmp::{ max, min };
 use specs::prelude::*;
 
-const MAPHEIGHT : usize = 43;
-const MAPWIDTH : usize = 80;
-const MAPCOUNT : usize = MAPHEIGHT * MAPWIDTH;
+pub const MAPHEIGHT : usize = 43;
+pub const MAPWIDTH : usize = 80;
+pub const MAPCOUNT : usize = MAPHEIGHT * MAPWIDTH;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum TileType {
