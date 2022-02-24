9e706e8964b1efa9d2464e26f5fdad38a0089803
Kyle H <kyle.hagad@gmail.com>
2022-02-24 15:15:50 -0700

    BSP Corridors

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -1,8 +1,8 @@
 use specs::prelude::*;
 use super::{
-  Map, Rect, TileType, Position,
-  spawner,
-  SHOW_MAPGEN_VISUALIZER,
+    Map, Rect, TileType, Position,
+    spawner,
+    SHOW_MAPGEN_VISUALIZER,
 };
 mod simple_map;
 use simple_map::SimpleMapBuilder;
@@ -12,14 +12,19 @@ mod common;
 use common::*;
 
 pub trait MapBuilder {
-  fn build_map(&mut self);
-  fn spawn_entities(&mut self, ecs : &mut World);
-  fn get_map(&mut self) -> Map;
-  fn get_starting_position(&self) -> Position;
-  fn get_snapshot_history(&self) -> Vec<Map>;
-  fn take_snapshot(&mut self);
+    fn build_map(&mut self);
+    fn spawn_entities(&mut self, ecs : &mut World);
+    fn get_map(&mut self) -> Map;
+    fn get_starting_position(&self) -> Position;
+    fn get_snapshot_history(&self) -> Vec<Map>;
+    fn take_snapshot(&mut self);
 }
 
 pub fn random_builder(new_depth : i32) -> Box<dyn MapBuilder> {
-  Box::new(BspDungeonBuilder::new(new_depth))
+    let mut rng = rltk::RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1,1);
+    match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth)),
+    }
 }
