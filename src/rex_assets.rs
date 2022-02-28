7e8633a0f9776bec99b4e103c8ad11cb6d8feeaf
khagadorn <xekyle@gmail.com>
2022-02-28 13:55:18 -0700

    Add alt menu screen

--- src/rex_assets.rs
+++ src/rex_assets.rs
@@ -1,15 +1,18 @@
 use rltk::{ rex::XpFile };
 
-rltk::embedded_resource!(SMALL_DUNGEON, "../resources/cubecircles.xp");
+rltk::embedded_resource!(SMALL_DUNGEON, "../resources/SmallDungeon_80x50.xp");
+// rltk::embedded_resource!(SMALL_DUNGEON, "../resources/cubecircles.xp");
 
 pub struct RexAssets { pub menu : XpFile }
 impl RexAssets {
     #[allow(clippy::new_without_default)]
     pub fn new() -> RexAssets {
-        rltk::link_resource!(SMALL_DUNGEON, "../resources/cubecircles.xp");
+        rltk::link_resource!(SMALL_DUNGEON, "../resources/SmallDungeon_80x50.xp");
+        // rltk::link_resource!(SMALL_DUNGEON, "../resources/cubecircles.xp");
 
         RexAssets {
-            menu : XpFile::from_resource("../resources/cubecircles.xp").unwrap()
+            menu : XpFile::from_resource("../resources/SmallDungeon_80x50.xp").unwrap()
+            // menu : XpFile::from_resource("../resources/cubecircles.xp").unwrap()
         }
     }
 }
